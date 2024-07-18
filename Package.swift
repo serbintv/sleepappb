b063fc01deb1390147f81d393b8299640b57fe92
Taras Serbin <taras.serbin@deviark.com>
2024-07-18 18:13:58 +0300

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "SleepApp",
     dependencies: [
         // 💧 A server-side Swift web framework.
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.5"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.2.0"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc"),
         .package(url: "https://github.com/vapor/auth.git", from: "2.0.0-rc"),
         .package(url: "https://github.com/vapor/crypto.git", from: "3.0.0"),
